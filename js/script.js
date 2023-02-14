f4ef97b788c461b6b6cc2294b921acaa591372dd
uriapete <uriarteangelo@gmail.com>
2023-02-13 18:03:46 -0800

    Completed jQuery scripting for adding and removing skills

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 // alert("ready")
 console.log("script: ready");
 console.log("jQ:", $);
+
+$("button").click(function (e) { 
+    // e.preventDefault();
+    $("ul").append(`<li><span>X</span>${$("input").val()}</li>`);
+    $("input").val("");
+});
+
+$("ul").on("click", "span", function () {
+    $(this).closest("li").remove();
+});
